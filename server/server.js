92c272e8eddbb3714bd5a9357867841c28520a66
woodnathaniel <nathanielwood002@gmail.com>
2024-07-13 05:41:32 +0000

    crossorigin

--- server/server.js
+++ server/server.js
@@ -58,13 +58,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: "GET, POST, PUT, DELETE",
-//     credentials: true
-//   })
-// )
+app.use(
+  cors({
+    origin: "https://rockviewhospitalities.vercel.app",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+  })
+)
 
 
 
