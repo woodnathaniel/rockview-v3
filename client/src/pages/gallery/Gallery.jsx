7187ab35ce269f049d79f4ba82c06faea1f14f97
woodnathaniel <nathanielwood002@gmail.com>
2024-07-25 03:46:41 +0000

    gallery finished

--- client/src/pages/gallery/Gallery.jsx
+++ client/src/pages/gallery/Gallery.jsx
@@ -90,7 +90,11 @@ const Gallery = () => {
     "  https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/paga-crocodiles-46.jpg?alt=media&token=31032bf2-2c24-4ef8-86e4-c43c65d36f61",
     "  https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/tongo-whistling-rocks.jpg?alt=media&token=2a59d155-af02-439d-b34c-607de53e88fd",
     "  https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/villaggio.jpg?alt=media&token=4ea87da3-1790-4c91-89e2-53c2e83fca99",
-    "  https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/tourist.jpg?alt=media&token=fbd19fd5-8c6d-42b0-899f-83aa2e646631"
+    "  https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/tourist.jpg?alt=media&token=fbd19fd5-8c6d-42b0-899f-83aa2e646631",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/4171_Naa-gbewaa-shrine.jpg?alt=media&token=79a9449e-f147-4df5-bd32-20519f0c3e3f",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/images.jpg?alt=media&token=84b6eac3-fd6f-46ef-8f15-1b0964f60ed4",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/8230621e52984c0dae4cb627a8063fc0.jpg?alt=media&token=fcf13324-e9f2-4322-aa3a-3b7350572f7a",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/caption.jpg?alt=media&token=83fac718-ca70-409a-ae24-8ba9543fa8f3"
     ]
 
 
@@ -98,7 +102,11 @@ const Gallery = () => {
       "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/44023306_1781710075267390_515539566082392064_n.jpg?alt=media&token=92989a31-b42d-41a7-81eb-6fdc67ac2e79",
       "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/44052541_1781710098600721_5230673891923329024_n.jpg?alt=media&token=d4603f4e-96ee-4d47-ae96-d1a42477aac0",
       "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/Damba-Festival.webp?alt=media&token=249cebc7-d4c8-489f-8eac-ce570d041689",
-      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/Social-Basket-Bags-.jpg?alt=media&token=2f7219bf-91c9-4416-8c02-0754f95dde7b"
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/Social-Basket-Bags-.jpg?alt=media&token=2f7219bf-91c9-4416-8c02-0754f95dde7b",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/images.jpg?alt=media&token=84b6eac3-fd6f-46ef-8f15-1b0964f60ed4",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/43788701_1781709848600746_2146909608441020416_n.jpg?alt=media&token=38236767-4627-406f-8eed-0bfbee01cf0c",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/8230621e52984c0dae4cb627a8063fc0.jpg?alt=media&token=fcf13324-e9f2-4322-aa3a-3b7350572f7a",
+      "https://firebasestorage.googleapis.com/v0/b/todofirebase-df58c.appspot.com/o/basketseller-upper-east-region.jpg?alt=media&token=4bc0ce08-f84d-44db-ace0-52d38edddde2"
       ]
 
 
